3735b1d44abd63510edc6c7d160f85308cd9f355
“c-moX” <cmoxsh@gmail.com>
2021-06-10 11:49:03 +0600

    updated the payment layout page

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -4,9 +4,11 @@ import Image1 from '../../images/product-pic/bkash.png';
 import Image2 from '../../images/product-pic/nagad.png';
 import Image3 from '../../images/product-pic/payoneer.png';
 import Image4 from '../../images/product-pic/paypal.png';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, processOrder } from '../../utilities/databaseManager';
+import { useHistory } from 'react-router';
 
 const Payment = () => {
+    const history = useHistory();
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
@@ -22,37 +24,83 @@ const Payment = () => {
     }
     let subTotal = total / 50 + total;
 
+    // .....
+    // Proceed Checkout eventHandler func
+    const handleOrderConfirm = () => {
+        if (cart.length === 0) {
+            alert('Please! Continue shopping after follow this way.');
+        }
+        else {
+            const myModal = document.getElementById("myModal");
+            myModal.style.display = "block";
+        }
+    }
+
+    // Success button func:
+    const handleSuccessBtn = () => {
+        const myModal = document.getElementById("myModal");
+        myModal.style.display = "none";
+        alert('Congratulation! Your order has been confirm.....!!');
+
+        cart.length = 0;
+        let newCart = [...cart];
+        setCart(newCart);
+        processOrder(cart);
+        // Mongodb cart product delete func here...!!
+        history.push('/home');
+    }
+
+
+    // Wrong button func:
+    const handleWrongBtn = () => {
+        const myModal = document.getElementById("myModal");
+        myModal.style.display = "none";
+    }
+
     return (
         <div>
+            {/* Modal contents */}
+            <div id="myModal" className="modal" style={{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) )` }}>
+                <div className="modal-body">
+                    <h2>Welcome! Are you sure confirm your order.....??</h2>
+                    <button onClick={handleSuccessBtn} className="confirm-btn">Confirm</button>
+                    <button onClick={handleWrongBtn} className="cancel-btn">Cancel</button>
+                </div>
+            </div>
+
+            {/* Payments method */}
             <div className="payment-container">
                 <div className="payment-method">
                     <h1>Select payment method</h1>
-                    <div className="select-payment">
-                        <img src={Image1} className="item-box" alt="" />
-                        <img src={Image2} className="item-box" alt="" />
-                        <img src={Image3} className="item-box" alt="" />
-                        <img src={Image4} className="item-box" alt="" />
-                    </div>
+                    <ul className="select-payment">
+                        <li><img src={Image1} className="item-box" alt="" /></li>
+                        <li><img src={Image2} className="item-box" alt="" /></li>
+                        <li><img src={Image3} className="item-box" alt="" /></li>
+                        <li><img src={Image4} className="item-box" alt="" /></li>
+                    </ul>
                 </div>
 
                 <div className="payment-summary">
                     <h1>Order Summary</h1>
-                    <h3 className="payment-info">
-                        <span>Total Items</span>
-                        <span className="right">{cart.length} psc</span>
-                    </h3>
-                    <h3 className="payment-info">
-                        <span>Total Amount (1 items and shipping fee included)</span>
-                        <span className="right">${subTotal}</span>
-                    </h3>
+                    <div>
+                        <article className="payment-info">
+                            <strong>Subtotal Items</strong>
+                            <span className="right">{cart.length} psc</span>
+                        </article>
+                        <article className="payment-info">
+                            <span><strong>Total Amount </strong>(1 items and shipping fee included)</span>
+                            <span className="right">${subTotal}</span>
+                        </article>
+                    </div>
                 </div>
             </div>
 
+            {/* Payment details */}
             <div style={{ textAlign: 'center' }} className="pay-area">
                 <h2 style={{ borderBottom: '1px solid #e4dbec' }}>Developer is Sleeping!!</h2>
                 <h3 style={{ color: 'green' }}>Please! wait, Your order is proccesing</h3>
                 <h1 style={{ paddingTop: '10px' }}>You are welcome to continue shopping!</h1>
-                <button type="submit">Order Confirm</button>
+                <button onClick={handleOrderConfirm} type="submit">Order Confirm</button>
             </div>
         </div>
     );
